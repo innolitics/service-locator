a82622a810c0eb4f5b2d0f65eedba8c20f2d84ec
Yujan Shrestha <ricebowl1988@gmail.com>
2013-08-11 04:51:40 -0500

    add files for pip distribution

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='ServiceLocator',
+    name='service-locator',
     version='0.1.3',
     packages=['servicelocator',],
     license='Apache License',
     description='Dead simple python service locator',
     long_description=open('README.rst').read(),
     author="Yujan Shrestha",
-    author_email="yshrestha88@gmail.com"
+    author_email="yshrestha88@gmail.com",
+    url="https://github.com/eipiconsulting/service-locator"
 )
